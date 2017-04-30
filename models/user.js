e1841e609e02ae0f91671204fe5d3ebe0be5ac6b
Teodor Garzdin <teodorgarzedin@gmail.com>
2017-04-30 11:52:17 +0200

    Refine the user model by hashing the password before saving it and adding a method on the schema to compare plain-text passwords with hashed ones.

--- models/user.js
+++ models/user.js
@@ -1,12 +1,46 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+var SALT_FACTOR = 10;
 
-var userSchema = new Schema({
-  email:  String,
-  password: String,
-  registered_on: { type: Date, default: Date.now }
+var Schema = new Schema({
+  email:  {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  registered_on: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-var User = mongoose.model('User', userSchema);
+Schema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(error, salt) {
+    if (error) return next(error);
+    bcrypt.hash(user.password, salt, function(error, hash) {
+      if (error) return next(error);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+Schema.methods.comparePassword = function(candidatePassword, callback) {
+  bcrypt.compare(candidatePassword, this.password, function(error, isMatch) {
+    if (error) return callback(error);
+    callback(null, isMatch);
+  });
+};
+
+var User = mongoose.model('User', Schema);
 
 module.exports = User;
